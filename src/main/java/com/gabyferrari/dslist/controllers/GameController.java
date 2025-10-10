794e8055e54df6588335bc1b63d2aa3b6a6472cb
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-10-10 16:38:37 -0300

    GameDTO + GET game by id

--- src/main/java/com/gabyferrari/dslist/controllers/GameController.java
+++ src/main/java/com/gabyferrari/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gabyferrari.dslist.dto.GameDTO;
 import com.gabyferrari.dslist.dto.GameMinDTO;
 import com.gabyferrari.dslist.services.GameService;
 
@@ -23,4 +25,9 @@ public class GameController {
 		return result;
 	}
 
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = service.findById(id);
+		return result;
+	}
 }
--- src/main/java/com/gabyferrari/dslist/services/GameService.java
+++ src/main/java/com/gabyferrari/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gabyferrari.dslist.dto.GameDTO;
 import com.gabyferrari.dslist.dto.GameMinDTO;
 import com.gabyferrari.dslist.entities.Game;
 import com.gabyferrari.dslist.repositories.GameRepository;
@@ -15,9 +17,15 @@ public class GameService {
 	@Autowired
 	private GameRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = repository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = repository.findById(id).get();
+		return new GameDTO(result);
+	}
 }
