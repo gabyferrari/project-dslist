af90a7fc077368cbe771ecd77f323bb9503e88ba
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-10-21 17:48:56 -0300

    Moving games on list

--- src/main/java/com/gabyferrari/dslist/services/GameListService.java
+++ src/main/java/com/gabyferrari/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.gabyferrari.dslist.dto.GameListDTO;
 import com.gabyferrari.dslist.entities.GameList;
+import com.gabyferrari.dslist.projections.GameMinProjection;
 import com.gabyferrari.dslist.repositories.GameListRepository;
+import com.gabyferrari.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,9 +18,27 @@ public class GameListService {
 	@Autowired
 	private GameListRepository repository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = repository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i<= max; i++) {
+			repository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
